1855f480e240b0a0912a3000faaecc09c48ae8ec
EC2 Default User <ec2-user@ip-172-31-2-143.ec2.internal>
2018-05-22 14:45:02 +0000

    ok

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,19 +3,23 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
-
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
      <div class="row">
-        <div class="col-xs-6">
-        {!! Form::label('content', 'タスク内容:') !!}
-        {!! Form::text('content') !!}
-    </div>
-    <div class="form-group">
-            {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-    </div>
+          <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            <div class="form-group">
+
+                {!! Form::label('content', 'タスク:') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
 
         {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        </div>
     
     {!! Form::close() !!}
     </div>
